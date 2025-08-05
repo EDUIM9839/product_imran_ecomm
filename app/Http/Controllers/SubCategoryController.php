7b69ad677a9bb9e9f668e5ffe64f37774b121a62
EDUIM9839 <mohdim9839@gmail.com>
2025-08-05 12:07:06 -0700

    crate sub category

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -6,5 +6,37 @@ use Illuminate\Http\Request;
 
 class SubCategoryController extends Controller
 {
-    //
+    public function index() {
+        $categories = Category::all();
+        return view('subcategories.index', compact('categories'));
+    }
+
+    public function list(Request $request) {
+        $subcategories = SubCategory::with('category')->select('sub_categories.*');
+        return datatables()->of($subcategories)
+            ->addColumn('checkbox', function($row){ /*...*/ })
+            ->addColumn('image', function($row){ /*...*/ })
+            ->addColumn('category_name', function($row){ return $row->category->name; })
+            ->editColumn('status', function($row){ /*...*/ })
+            ->addColumn('action', function($row){ /*...*/ })
+            ->rawColumns(['checkbox','image','status','action'])
+            ->make(true);
+    }
+
+    public function store(Request $request) {
+        // Validate & Save SubCategory
+    }
+
+    public function update(Request $request) {
+        // Validate & Update SubCategory
+    }
+
+    public function delete(Request $request) {
+        // Delete Single SubCategory
+    }
+
+    public function bulkDelete(Request $request) {
+        // Bulk Delete SubCategories
+    }
 }
+
